548dcd2f4c7ea7e7f00a18b7c73f9735674c553c
Simon Dittlmann <131621+Pindar@users.noreply.github.com>
2019-08-11 23:49:10 +0200

    feat: update

--- components/SDArticle.vue
+++ components/SDArticle.vue
@@ -4,7 +4,7 @@
       <h1 class="f2 lh-title fw9 mb3 mt0 pt3 bt bw2">
         {{ blog.attributes.title }}
       </h1>
-      <time class="f6 ttu tracked gray">{{ date }}</time>
+      <time class="f6 ttu tracked mid-gray">{{ date }}</time>
     </header>
     <div class="fn fl-ns w-50-ns">
       <div class="f5 lh-copy measure mt0-ns">
