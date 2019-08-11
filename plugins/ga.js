ab735c1ea3a9c65f16f4ab4ae06ebae40dadebdc
Simon Dittlmann <131621+Pindar@users.noreply.github.com>
2019-08-11 22:56:49 +0200

    feat: global site tag

--- plugins/ga.js
+++ plugins/ga.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 
 export default ({ app }) => {
+  const GA_MEASUREMENT_ID = 'UA-27470524-1'
   /*
   ** Only run on client-side and only in production mode
   */
@@ -11,7 +12,7 @@ export default ({ app }) => {
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.googletagmanager.com/gtag/js?id=UA-27470524-1','ga');
+  })(window,document,'script','https://www.googletagmanager.com/gtag/js?id=' + GA_MEASUREMENT_ID,'ga');
   /*
   ** Set the current page
   */
@@ -19,7 +20,7 @@ export default ({ app }) => {
  function gtag(){dataLayer.push(arguments);}
  gtag('js', new Date());
 
- gtag('config', 'UA-27470524-1', { 'optimize_id': 'GTM-WBMC8S6', 'anonymize_ip': true});
+ gtag('config', GA_MEASUREMENT_ID, { 'optimize_id': 'GTM-WBMC8S6', 'anonymize_ip': true});
   /*
   ** Every time the route changes (fired on initialization too)
   */
@@ -27,7 +28,6 @@ export default ({ app }) => {
     /*
     ** We tell Google Analytics to add a `pageview`
     */
-    ga('set', 'page', to.fullPath)
-    ga('send', 'pageview')
+    gtag('config', GA_MEASUREMENT_ID, {'page_path': to.fullPath});
   })
 }
